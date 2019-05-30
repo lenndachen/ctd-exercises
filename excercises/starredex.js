2b3949c3d71f7f099341c1dbffebe1599177aac1
Lennda Chen <lindachen548@gmail.com>
2019-05-30 16:43:20 -0400

    trying 2-2000

---
+++ excercises/starredex.js
@@ -0,0 +1,47 @@
+//Palindrome
+
+
+
+
+
+//Separate Dogs
+
+
+
+
+
+
+//Pig Latin
+
+
+
+
+
+
+//Recipe and food determination
+
+
+
+
+
+
+//Print every prime # from 2 to 2,000
+function print2to2000(x)
+for (i = 2; i < 2000; i++);{
+    text += "The number is" +i+ "<br>";
+
+if((x+1)%2==0){
+    print(x + "is not prime");
+}else{
+    print(x + "is not prime");
+}
+}
+
+//Two strings-return largest common substring from 2 strings
+
+
+
+
+
+
+//Given two lists-each list is a subset of another list
